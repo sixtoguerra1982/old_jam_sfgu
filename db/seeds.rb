a43e9592ce2f49c3cc89cf03c7d4117d20b41f54
sbstn <sbstn.jmnz@yahoo.com>
2019-02-17 15:12:43 -0300

    basic User/Post/Comment structure working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Comment.delete_all
+Post.delete_all
+User.delete_all
+
+User.create([{ name: 'Alice' }, { name: 'Bob'}, {name: 'James'}])
+
+10.times do
+  rand = rand(0..7)
+  user = User.order('RANDOM()').first
+  post = Post.create(
+    title: Faker::Lorem.sentence(3),
+    content: Faker::Lorem.paragraphs,
+    image: Faker::LoremPixel.image('300x200'),
+    user: user
+  )
+  rand.times do
+    post.comments.create(
+      content: Faker::Lorem.sentence(3),
+      user: User.where.not(id: user.id).order('RANDOM()').first
+    )
+  post.save
+  end
+end
